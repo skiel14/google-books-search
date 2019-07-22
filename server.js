a713d0b66adc38296e6c89d8b5dd98c168ec801f
skiel <platypony14@gmail.com>
2019-07-21 22:34:48 -0400

    heroku changes for mongo

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 
 // Define middleware here
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -16,14 +15,32 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
+// Connect to the Mongo DB
 mongoose.connect(
-  "mongodb://skiel:abc123abc@ds351107.mlab.com:53537/heroku_dkjqnstq",
-  { useNewUrlParser: true }
+  "mongodb://skiel:abc123abc@ds253537.mlab.com:53537/heroku_dkjqnstq",
+  {
+    // useCreateIndex: true,
+    useNewUrlParser: true
+  }
 );
 
-//mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+// mongoose.connect(
+//   process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
+//   {
+//     useCreateIndex: true,
+//     useNewUrlParser: true
+//   }
+// );
 
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
+
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+
+// // Start the API server
+// app.listen(process.env.PORT, function() {
+//   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+// });
+//
