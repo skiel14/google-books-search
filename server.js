49f00db79e413486c9fc8afc0311e29620c4cea4
skiel <platypony14@gmail.com>
2019-07-21 20:51:39 -0400

    fix --force

--- server.js
+++ server.js
@@ -16,7 +16,12 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+mongoose.connect(
+  "mongodb://skiel:abc123abc@ds351107.mlab.com:51107/heroku_dkjqnstq",
+  { useNewUrlParser: true }
+);
+
+//mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
 
 // Start the API server
 app.listen(PORT, function() {
--- src/components/Form/index.js
+++
@@ -1,3 +0,0 @@
-export * from "./Input";
-export * from "./FormBtn";
-export * from "./Label";
