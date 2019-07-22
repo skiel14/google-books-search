99a180b8e138d1f318ac6a8e16e73d23783a8712
skiel <platypony14@gmail.com>
2019-07-22 00:28:31 -0400

    testing routes

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ router.use(function(req,res,next) {
 });
 
 // API Routes
-router.use("/api", apiRoutes);
+router.use("/api/", apiRoutes);
 
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
